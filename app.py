5fcabe34e4ffa65f68f846af026753597ded19d7
Miro Frankovic <miro.frankovic@gmail.com>
2019-03-13 20:25:58 +0000

    initialized recipes file

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import os
-from flask import Flask, render_template, url_for, redirect
-
+from flask import Flask, render_template, url_for, redirect, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'cookbook_trisport'
+app.config["MONGO_URI"] = 'mongodb://<dbuser>:<dbpassword>@ds159185.mlab.com:59185/cookbook_trisport'
+
+
+mongo = PyMongo(app)
+
+
 
 
 @app.route('/')
-@app.route('/workout')
-def workout():
+@app.route('/nutrition')
+def nutrition():
     return render_template('index.html',
-    )
+                          recipes = mongo.db.recipes.find())
+                          
+@app.route('/get_recipes') 
+def get_recipes():
+    return render_template('recipes.html',
+                           recipes = mongo.db.recipes.find())
     
     
 if __name__ == '__main__':
