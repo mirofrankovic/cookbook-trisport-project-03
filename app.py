36d144ee4cabc16500337e6dbf42800554beece4
Miro Frankovic <miro.frankovic@gmail.com>
2019-04-17 19:32:40 +0000

    created edit file

--- app.py
+++ app.py
@@ -11,27 +11,23 @@ app.config["MONGO_DBNAME"] = 'cookbook_trisport'
 app.config["MONGO_URI"] = 'mongodb://mirof:Vanilia123@ds159185.mlab.com:59185/cookbook_trisport'
 
 
-mongo = PyMongo(app)
-
+mongo = PyMongo(app)             #constructor method
+ 
 
 
 
 @app.route('/')
 @app.route('/get_ready')
 def get_ready():
-    return render_template('getready.html',
-                          recipes = mongo.db.recipes.find()) #supply recipes collection
+    return render_template('getready.html', recipes=mongo.db.recipes.find()) #supply recipes collection
                           
                           
-                          
-@app.route('/get_recipes') # images for my recepies
+@app.route('/get_recipes') # images for my recepies and see all my recipes after adding them
 def get_recipes():
-    return render_template('recipes.html',
-                          recipes = mongo.db.recipes.find())
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
                           
-                          
-                          
-@app.route('/add_recipe')
+
+@app.route('/add_recipe')  #only for display my recipes in my form
 def add_recipe():
     
 # {
@@ -48,35 +44,34 @@ def add_recipe():
 #    "vegan_type_meal": "",
 #    "due_date": ""
 #}   
+
     return render_template('add_recipe.html',
-                           author_name=mongo.db.author_name.find(),
-                           meal_type=mongo.db.meal_type.find(),
-                           sport_type=mongo.db.sport_type.find(),
-                           race_day=mongo.db.race_day.find(),
-                           vegan_meal=mongo.db.vegan_meal.find()
+                          recipe={},
+                          author_name=mongo.db.author_name.find(),
+                          meal_type=mongo.db.meal_type.find(),
+                          sport_type=mongo.db.sport_type.find(),
+                          race_day=mongo.db.race_day.find(),
+                          vegan_meal=mongo.db.vegan_meal.find(),
+                          recipes=mongo.db.recipes.find()
                           )
                           
                           
     
                           
                           
-@app.route('/insert_my_recipe/<recipe_id>', methods=['POST'])    #edit_recipe
+@app.route('/insert_my_recipe/<recipe_id>', methods=['POST'])    #edit_recipe to my database
 def insert_my_recipe(recipe_id):
     this_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template('insert_my_recipe.html', recipe=this_recipe)
 
 
 
-
-
-
-
 @app.route('/submit_to_database', methods=['POST'])
 def submit_to_database():
+    print(request.form.to_dict())
     recipes=mongo.db.recipes
-    
     recipes.insert_one(request.form.to_dict())     #or (request.json)
-    return redirect(url_for(add_recipe.html))
+    return redirect(url_for('get_recipes'))
                                                     #return('', 204) # what is 204
     
                           
@@ -124,9 +119,6 @@ def log_in():
                                    recipes=recipes_dics)
         
         
-        
-        
-      
 @app.route('/my_recipes', methods=['GET', 'POST'])
 def my_recipes():
     if not 'username' in session:
@@ -138,16 +130,10 @@ def my_recipes():
                            recipes=recipes)                         
 
 
-
-
 @app.route('/contact_us')
 def contact_us():
     return render_template("contactus.html")
                           
-                          
-                           
-                           
-    
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
