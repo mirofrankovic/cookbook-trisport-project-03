47c5a3e20d01e284d9270537fc136210b37ddc1a
Miro Frankovic <miro.frankovic@gmail.com>
2020-12-01 22:45:30 +0000

    Delete env.py

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("MONGO_URI", "mongodb+srv://miro:Qjkq0ov8uwnPONeV@mirocluster.y2pt0.mongodb.net/cookbook_trisport?retryWrites=true&w=majority")
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY",  "h(M4Oq`(H[x_XX;P,{cl&c;xMOG:D")
-os.environ.setdefault("MONGO_DBNAME", "cookbook_trisport")
